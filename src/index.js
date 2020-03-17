70f899430ce354c2fea57ada87208b7d1995e095
vanweerden <ando.vanweerden@gmail.com>
2020-03-17 09:30:10 +0800

    Score, user, and time added to Card display

--- src/index.js
+++ src/index.js
@@ -5,30 +5,46 @@ import './styles.css';
 // Items are accessed like so: https://hacker-news.firebaseio.com/v0/item/<id>.json
 // Array of top 500 stories: https://hacker-news.firebaseio.com/v0/topstories.json
 
-// Will present news item info prettily
-class Card extends React.Component {
-  render() {
-    return
-    <div>
+// Presents info of one news item
+function Card(props) {
+  const item = props.item;
 
+  // Calculates ms since it was posted (stored as UNIX timestamp)
+  const ms = (Date.now() - item.time * 1000);
+  const min = Math.floor((ms / 1000) / 60);
+  const hr = Math.floor(min / 60);
+
+  // If posted less than an hour ago, post minutes, else hours
+  const time = min < 60 ? min + " minutes"
+                        // Handle plurality of hours
+                        : hr === 1 ? + hr + " hour"
+                                     : hr + " hours";
+
+  return (
+    <div className="card">
+      <div className="title"><span className="rank">{props.rank}</span><a href={item.url} target='_blank' className="storylink">{item.title}</a><span className='website'></span></div>
+
+      <div className="subtitle"><span className="score">{item.score} points</span><span className="user"> by {item.by}</span><span className="time"> {time} ago</span></div>
     </div>
-  }
+  );
+
 }
 
 // List of all news items: eventually use Card component to do this
 function List(props) {
   const items = props.items;
   const limit = 30;  // Number of items to display on page
+
+  // Passes each news item JSON object to Card component
   const listItems = items.map((item, i) => {
     if (i < limit) {
-      return <li key={item.id}>{item.title}</li>
+      return <Card item={item}
+                   rank={i + 1} // Rank on website (based on array index)
+                   key={item.id} />
     }
   });
   return (
-    <div>
-      <h1>Top News Stories</h1>
-      <ol>{listItems}</ol>
-    </div>
+    <div>{listItems}</div>
   )
 }
 
